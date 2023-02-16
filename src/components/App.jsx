8112aad2dc18ce23285846dc79fea7a682d2f89d
StaloneSerhii <sergio.fly4405@gmail.com>
2023-02-16 22:24:08 +0200

    4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,17 @@ import data from '../data/data.json'
 import { FriendList } from './friend/FriendList';
 import friends from '../data/friends.json'
 
+import {TransactionHistory} from './transaction/TransactionHistory'
+import  transaction  from '../data/transactions.json';
+
+
 export const App = () => {
   return (
     <div>
       <Profile items={user} />
       <Statistics items={data} />
-      <FriendList items = {friends} />
+      <FriendList items={friends} />
+      <TransactionHistory items={transaction}/>
     </div>
   );
 };
