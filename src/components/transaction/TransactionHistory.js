882855bd0277981c71a06ab465802dc4c43d715a
StaloneSerhii <sergio.fly4405@gmail.com>
2023-02-18 20:11:30 +0200

    style

--- src/components/transaction/TransactionHistory.js
+++ src/components/transaction/TransactionHistory.js
@@ -1,24 +1,25 @@
+import { Transaction, TransactionTable } from './TransactionHistory.style';
 
-export const TransactionHistory = ({items}) => {
-    return (
-      <table className="transaction-history">
-        <thead>
+export const TransactionHistory = ({ items }) => {
+  return (
+    <Transaction>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <TransactionTable>
+        {items.map(item => (
           <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
-          </tr>
-        </thead>
-
-        <tbody>
-                {items.map(item => (
-            <tr>
             <td>{item.type}</td>
             <td>{item.amount}</td>
             <td>{item.currency}</td>
           </tr>
         ))}
-        </tbody>
-      </table>
-    )
-}
+      </TransactionTable>
+    </Transaction>
+  );
+};
--- src/components/transaction/TransactionHistory.style.jsx
+++ src/components/transaction/TransactionHistory.style.jsx
@@ -1,3 +1,29 @@
 import styled from '@emotion/styled';
 
-export const Transaction = styled.ul``;
+export const Transaction = styled.table`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  thead {
+    border: 1px solid black;
+    background-color: #00bcd5;
+    padding: 10px 96px;
+    tr {
+      th {
+        padding-left: 10px;
+      }
+    }
+  }
+`;
+
+export const TransactionTable = styled.table`
+  background-color: gainsboro;
+  border: 1px solid black;
+  tr {
+    td {
+      padding: 10px 34px;
+      color: gray;
+    }
+  }
+`;
